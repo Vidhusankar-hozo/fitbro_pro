42737ae36bfa717cc1b9fa26108a298cc80d3ef0
Vidhusankar-hozo <vidhusankar11@gmail.com>
2025-06-19 16:46:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,13 @@ st.subheader("🚀 Quick Access")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.page_link("pages/2_Workout_Logger.py", label="🏋️ Log Workout")
-    st.page_link("pages/4_Meal_Tracker.py", label="📈 Track Meals")
-    st.page_link("pages/6_Sleep_and_Mood_Tracker.py", label="😴 Sleep & Mood")
+    st.page_link("pages/1_🏋️_Workouts.py", label="🏋️ Log Workout")
+    st.page_link("pages/2_🍱_Meals.py", label="🍱 Meals & Calories")
+    st.page_link("pages/4_😴_Sleep_Mood.py", label="😴 Sleep & Mood")
 
 with col2:
-    st.page_link("pages/3_Meal_Logger.py", label="🍽️ Log Meal")
-    st.page_link("pages/5_Weight_Tracker.py", label="⚖️ Weight Tracker")
-    st.page_link("pages/5_ChatBot.py", label="🤖 Ask FitBro")
+    st.page_link("pages/3_📉_Weight.py", label="📉 Track Weight")
+    st.page_link("pages/5_🤖_ChatBot.py", label="🤖 Ask FitBro")
 
 st.divider()
 st.markdown("""
